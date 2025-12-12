c6fe144321cd92e03c2ae207a99ae5fe8dee8dd8
ChiluverchethanKumar <chiluverichethankumar@gmail.com>
2025-12-12 06:52:11 +0530

    Removed submodule and added Zometric source files

--- Zometric/service.js
+++
@@ -1,8 +0,0 @@
-// service.js
-import TrackPlayer from 'react-native-track-player';
-
-module.exports = async function () {
-  // keep empty for now; required so module works
-  TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
-  TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
-};
--- Zometric/src/navigation/index.tsx
+++
@@ -1,5 +0,0 @@
-// src/navigation/index.ts
-import RootNavigator from './RootNavigator';
-import TabNavigator from './TabNavigator';
-
-export { RootNavigator, TabNavigator };
